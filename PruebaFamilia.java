84621949f2c61cf539023be285b9d667bc09760d
David Urtecho <106795516+Dafer3013@users.noreply.github.com>
2022-07-13 10:24:11 -0600

    Add files via upload

---
+++ PruebaFamilia.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class PruebaFamilia {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        Scanner input1 = new Scanner(System.in);
+        Scanner input2 = new Scanner(System.in);
+
+        Persona person;
+        Familia fam;
+        Direccion direction;
+
+        String nom, genero, nacimiento, id, rol, casa, calle, barrio, ciudad;
+        for (int x = 0; x < 2; x++) {
+            System.out.println(" ");
+            System.out.print("Ingrese el nombre de la persona: ");
+            nom = input.next();
+
+            System.out.print("Ingrese la identidad: ");
+            id = input.next();
+
+            System.out.print("La fecha del nacimiento: ");
+            nacimiento = input.next();
+
+            System.out.print("Ingrese su genero: ");
+            genero = input.next();
+
+            System.out.print("Ingrese su rol en la familia: ");
+            rol = input1.next();
+
+            System.out.print("Ingrese su # de casa:  ");
+            casa = input.next();
+
+            System.out.print("Ingrese la calle: ");
+            calle = input.next();
+
+            System.out.print("Ingrese el barrio de la persona: ");
+            barrio = input1.next();
+
+            System.out.print("Ingrese la ciudad de la persona: ");
+            ciudad = input2.next();
+
+            person = new Persona(nom, genero, nacimiento, id);
+            fam = new Familia(rol, person);
+            direction = new Direccion(casa, calle, barrio, ciudad, fam);
+
+            System.out.println(" ");
+            System.out.println(" ******************************* Impresion de Datos *******************************");
+            System.out.print(direction);
+            System.out.println(" ");
+        }
+    }
+}
